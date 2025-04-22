54788dee962b30130d4ee802487f9014afc9df28
GruberElisabeth <gruber.elisabethh@gmail.com>
2025-04-22 09:37:34 +0200

    added Model/ParkingStation/Main

---
+++ project_ssa/src/main/java/org/parking/Main.java
@@ -0,0 +1,13 @@
+package org.parking;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		getUserInput();
+//		getPrediction();
+//		getData();
+//		saveData();
+//		readData();
+	}
+
+}
---
+++ project_ssa/src/main/java/org/parking/ParkingStationModel.java
@@ -0,0 +1,29 @@
+package org.parking;
+
+import java.time.LocalDateTime;
+
+public class ParkingStationModel implements Model {
+	public LocalDateTime date;
+	public LocalDateTime startDate;
+	public LocalDateTime endDate;
+	public Object[] data;
+
+	@Override
+	public void modelData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getPrediction(LocalDateTime date) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void getPrediction(LocalDateTime startDate, LocalDateTime endDate) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
